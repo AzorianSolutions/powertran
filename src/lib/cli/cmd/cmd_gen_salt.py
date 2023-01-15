6294924001aae1b98ee5c7cbfcc1134990d9d4c0
Matt Scott <matt@azorian.solutions>
2023-01-15 09:33:35 -0500

    Tweaked CLI command annotations.

--- src/lib/cli/cmd/cmd_gen_salt.py
+++ src/lib/cli/cmd/cmd_gen_salt.py
@@ -3,8 +3,8 @@ from cryptography.fernet import Fernet
 from lib.cli.app import Environment, pass_environment
 
 
-@click.command("gen-salt", short_help="Generates a Fernet encryption key to be used for encrypting passwords.")
+@click.command("gen-salt", short_help="Generates a Fernet encryption key to be used for data encryption.")
 @pass_environment
 def cli(ctx: Environment):
-    """ Generates a Fernet encryption key to be used for encrypting passwords. """
+    """ Generates a Fernet encryption key to be used for data encryption. """
     print(f'Generated Key: {Fernet.generate_key().decode("utf-8")}')
